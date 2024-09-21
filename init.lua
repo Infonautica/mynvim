1ffb0571b9dadce96369f3f36e07ea57c4415f80
Leonid Danilov <leoniddanilov@storyteq.com>
2024-09-21 18:37:13 +0200

    Init

---
+++ init.lua
@@ -0,0 +1,15 @@
+-- Setting up the basic configurations
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
+-- Initiate Lazy package manager
+require("config.lazy")
+
+-- Setup a theme
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
+
+-- Setup mappings for all of the plugins
+require("config.mappings")
---
+++ lua/config/mappings.lua
@@ -0,0 +1,9 @@
+-- Setup telescope mappings
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = 'Find word' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
+
+-- Setup neo tree mappings
+vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { silent = true, noremap = true })
