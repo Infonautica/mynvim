9c52749e8f3b60b0b5bb8ceba5eea642d28f5ab9
Leonid Danilov <leoniddanilov@storyteq.com>
2024-09-30 16:27:10 +0200

    Updated theme, added volar LSP for Vue

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,31 +1,48 @@
 return {
-  {
-      "williamboman/mason.nvim",
-      config = function()
-        require("mason").setup()
-      end
-  },
-  {
-      "williamboman/mason-lspconfig.nvim",
-      config = function()
-        require("mason-lspconfig").setup({
-          ensure_installed = {
-            "lua_ls",
-            "ts_ls"
-          }
-        })
-      end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup {}
-      lspconfig.ts_ls.setup {}
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"ts_ls",
+					"volar",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.volar.setup({})
+			lspconfig.ts_ls.setup({
+				init_options = {
+					plugins = {
+						{
+							name = "@vue/typescript-plugin",
+							location = "/Users/leoniddanilov/.nodenv/versions/20.17.0/lib/node_modules/@vue/typescript-plugin",
+							languages = { "javascript", "typescript", "vue" },
+						},
+					},
+				},
+				filetypes = {
+					"javascript",
+					"typescript",
+					"vue",
+				},
+			})
 
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-      vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, {})
-    end
-  }
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+			vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
