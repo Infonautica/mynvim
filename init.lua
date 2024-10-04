1e12949ce22e5e37344abd9d1fc5a7cd43056a31
Leonid Danilov <leoniddanilov@storyteq.com>
2024-10-04 15:42:30 +0200

    Updated configuration variant

--- init.lua
+++ init.lua
@@ -1,9 +1,13 @@
 -- Setting up the basic configurations
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.opt.expandtab = true
+vim.opt.tabstop=2
+vim.opt.softtabstop=2
+vim.opt.shiftwidth=2
 vim.g.mapleader = " "
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+
 
 -- Initiate Lazy package manager
 require("config.lazy")
