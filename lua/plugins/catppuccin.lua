66d10f2b6ac65196d19945af3caa3de06586f7b3
Leonid Danilov <leoniddanilov@storyteq.com>
2024-12-10 14:26:24 +0100

    Theme configuration in progress

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,8 +1,25 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme "catppuccin-mocha"
-  end
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1500,
+	config = function()
+		local theme = require("catppuccin")
+		theme.setup({
+			treesitter = false,
+			integrations = {
+				alpha = true,
+				aerial = true,
+				mason = true,
+				neotree = true,
+				notify = true,
+				nvimtree = false,
+				semantic_tokens = true,
+				symbols_outline = true,
+				telescope = true,
+				ts_rainbow = false,
+				which_key = true,
+			},
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,18 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = { "lua", "javascript", "typescript" }
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			auto_install = true,
+			sync_install = true,
+			ensure_installed = { "lua", "javascript", "typescript", "vue" },
+			ignore_install = {},
+			modules = {
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+			},
+		})
+	end,
 }
