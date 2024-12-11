bdcedd0c954e77be270631188809893030554dd3
Leonid Danilov <leoniddanilov@storyteq.com>
2024-12-11 11:57:46 +0100

    Show more files in the tree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,25 +1,32 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    local neotree = require("neo-tree")
-    neotree.setup({
-      window = {
-        mappings = {
-          ["l"] = "open",
-          ["h"] = "close_node",
-        },
-      },
-      filesystem = {
-        follow_current_file = {
-          enabled = true,
-        },
-      },
-    })
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		local neotree = require("neo-tree")
+		neotree.setup({
+			window = {
+				mappings = {
+					["l"] = "open",
+					["h"] = "close_node",
+				},
+			},
+			filesystem = {
+				filtered_items = {
+          visible = true,
+					hide_dotfiles = false,
+					always_show_by_pattern = {
+						".env*",
+					},
+				},
+				follow_current_file = {
+					enabled = true,
+				},
+			},
+		})
+	end,
 }
