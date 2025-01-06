196a88d4025e39803dfdf9a4237a4a5e4d172057
Leonid Danilov <leoniddanilov@storyteq.com>
2025-01-06 15:39:36 +0100

    Show full path in lualine, hide other sections in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,19 @@
+local function filepath()
+	return vim.fn.expand("%:p")
+end
+
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "dracula"
-      }
-    });
-  end
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "dracula",
+			},
+			sections = {
+				lualine_c = { filepath },
+				lualine_x = {},
+				lualine_y = {},
+			},
+		})
+	end,
 }
