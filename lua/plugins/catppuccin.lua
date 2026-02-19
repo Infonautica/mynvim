9cae4b8e7cd7fb3a704dc3d9d74aca57a06b14d2
Leonid Danilov <leoniddanilov@storyteq.com>
2026-02-19 11:03:21 +0100

    Updated packages, used macchiato

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,31 +1,31 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1500,
-	config = function()
-		local theme = require("catppuccin")
-		theme.setup({
-			transparent_background = true,
-			term_colors = false,
-			background = {
-				light = "latte",
-				dark = "mocha",
-			},
-			treesitter = true,
-			integrations = {
-				alpha = true,
-				aerial = true,
-				mason = true,
-				neotree = true,
-				notify = true,
-				nvimtree = false,
-				semantic_tokens = true,
-				symbols_outline = true,
-				telescope = true,
-				ts_rainbow = false,
-				which_key = true,
-			},
-		})
-		vim.cmd.colorscheme("catppuccin-mocha")
-	end,
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1500,
+  config = function()
+    local theme = require("catppuccin")
+    theme.setup({
+      transparent_background = false,
+      term_colors = false,
+      background = {
+        light = "latte",
+        dark = "macchiato",
+      },
+      treesitter = true,
+      integrations = {
+        alpha = true,
+        aerial = true,
+        mason = true,
+        neotree = true,
+        notify = true,
+        nvimtree = false,
+        semantic_tokens = true,
+        symbols_outline = true,
+        telescope = true,
+        ts_rainbow = false,
+        which_key = true,
+      },
+    })
+    vim.cmd.colorscheme("catppuccin-macchiato")
+  end,
 }
